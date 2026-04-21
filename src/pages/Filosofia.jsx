954a9d51f755fd2c082c4ed4a1a13c88efa92b79
Mayra Alzate <nadja.minni@gmail.com>
2026-04-21 09:12:56 -0500

    "chore: script filosofia.jsx"

---
+++ src/pages/Filosofia.jsx
@@ -0,0 +1,18 @@
+
+
+const Filosofia = () => {
+    return (
+        <main>
+            <Header />
+            <section>
+                <h2><b>Menos es más</b></h2>
+                <p>Nuestra filosofía se centra en la observación y el respeto por los ritmos naturales. Creemos que cultivar un Bonsái no es solo jardinería, sino una meditación activa.
+
+                    En nuestro estudio, te enseñamos a escuchar al árbol, entendiendo que cada corte y cada alambrado debe tener un propósito. Buscamos el equilibrio perfecto entre la intervención humana y la expresión salvaje de la naturaleza.</p>
+            </section>
+            <Footer/>
+        </main>
+    )
+}
+
+export default Filosofia
